d262883db826dcbcd0246f2e69c260cc963e5950
Jason Dryhurst-Smith <jason@mint>
2013-11-14 20:52:15 +0000

    Changes to the spelling

--- UserScripts/xkcd-1288.0.1.js
+++ UserScripts/xkcd-1288.0.1.js
@@ -14,7 +14,7 @@ var n, walk=document.createTreeWalker(document.documentElement,NodeFilter.SHOW_T
 
 while(n=walk.nextNode()) {
 	n.textContent = n.textContent.replace(/witnesses/ig, 'these dudes I know');
-	n.textContent = n.textContent.replace(/witness/gi, 'this dude I know');
+	n.textContent = n.textContent.replace(/witness/ig, 'this dude I know');
 	n.textContent = n.textContent.replace(/allegedly/ig, "kinda probably");
 	n.textContent = n.textContent.replace(/new study/ig, 'tumblr post');
 	n.textContent = n.textContent.replace(/rebuild/ig, "avenge");
@@ -22,12 +22,10 @@ while(n=walk.nextNode()) {
 	n.textContent = n.textContent.replace(/google glass/ig, "vitual boy");
 	n.textContent = n.textContent.replace(/smartphones|smart phone/ig, "pokedex");
 	n.textContent = n.textContent.replace(/electric|elecrical/ig, "atomic");
-	n.textContent = n.textContent.replace(/senator|MP|councellor|MEP/ig, "elf lord");
+	n.textContent = n.textContent.replace(/senator|mp|councellor|mep/ig, "elf lord");
 	n.textContent = n.textContent.replace(/car/ig, "cat");
 	n.textContent = n.textContent.replace(/electionn/ig, "eating contest");
 	n.textContent = n.textContent.replace(/congressional leaders/ig, "river spirits");
 	n.textContent = n.textContent.replace(/homeland security/ig, "homestar runner");
-	n.textContent = n.textContent.replace(/could not be reached for comment/ig, "is guilty and everyone knows it");
-
-	// n.textContent = n.textContent.replace(/\b(?=[^\sf]*f)\S{4,}/g, function(match) { return match.replace(/f/g, "ph");});	
+	n.textContent = n.textContent.replace(/could not be reached for comment/ig, "is guilty and everyone knows it");	
 }
